034051dfde1e633242574997c5cf9bd5f1330829
Sergey <sergey.liquid@gmail.com>
2020-01-22 12:07:55 +0200

    second try

--- src/main/java/jek/collections/IntArrayList.java
+++ src/main/java/jek/collections/IntArrayList.java
@@ -1,4 +1,155 @@
 package jek.collections;
 
-public class IntArrayList {
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class IntArrayList implements List {
+
+    private int end;
+    private Object[] ial;
+
+    public IntArrayList() {
+        ial = new Object[10];
+        end = 0;
+    }
+
+    @Override
+    public int size() {
+        return end;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if (end == 0)
+            return true;
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        for (Object search : ial)
+            if (search.equals(o))
+                return true;
+
+        return false;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public boolean add(Object o) {
+        if(end == ial.length) {                             //чекаем, если конец равен длинне
+            Object[] ial2 = new Object[ial.length*2];       //создаем новый массив и копируем
+            for (int i = 0; i < ial.length; i++)
+                ial2[i] = ial[i];
+            ial = ial2;                                     //присваиваем ссылку старому парню
+        }
+        ial[end++] = o;                                     //добавляем обьект, увеличиваем конец
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Object get(int index) {
+
+        if (index >= end || index < 0)
+            throw new IndexOutOfBoundsException("ты пытаешься удалить то, чего у тебя нет!");
+
+        return ial[index];
+    }
+
+    @Override
+    public Object set(int index, Object element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, Object element) {
+
+    }
+
+    @Override
+    public Object remove(int index) {
+
+        if (index >= end || index < 0) {
+            throw new IndexOutOfBoundsException("ты пытаешься удалить то, чего у тебя нет!"); }
+
+
+     return null;
+    }
+
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List subList(int fromIndex, int toIndex) {
+        return null;
+    }
+
+    @Override
+    public boolean retainAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        return new Object[0];
+    }
 }
--- src/test/java/jek/collections/IntArrayListTest.java
+++ src/test/java/jek/collections/IntArrayListTest.java
@@ -1,5 +1,143 @@
+package jek.collections;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class IntArrayListTest {
 
+    @Test
+    void size() {
+        IntArrayList ial = new IntArrayList();
+        ial.add(1);
+        ial.add(5);
+        ial.add(54);
+
+        assertEquals(3, ial.size());
+    }
+
+    @Test
+    void isEmpty() {
+        IntArrayList ial = new IntArrayList();
+        ial.add(1);
+        ial.add(5);
+        ial.add(54);
+
+        assertFalse(ial.isEmpty());
+    }
+
+    @Test
+    void contains() {
+        IntArrayList ial = new IntArrayList();
+        ial.add(1);
+        ial.add(5);
+        ial.add(54);
+        ial.add(11);
+        ial.add(75);
+        ial.add(59);
+
+        assertTrue(ial.contains(54));
+    }
+
+    @Test
+    void iterator() {
+    }
+
+    @Test
+    void toArray() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void addAll() {
+    }
+
+    @Test
+    void testAddAll() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void getShouldThrowException() {
+        IntArrayList ial = new IntArrayList();
+        ial.add(1);
+        ial.add(5);
+        ial.add(54);
+
+        String expectedMessage = "ты пытаешься удалить то, чего у тебя нет!";
+
+        try {
+            ial.get(55);
+        } catch (IndexOutOfBoundsException thrown) {
+            assertEquals(expectedMessage, thrown.getMessage());
+        }
+}
+
+    @Test
+    void getShouldReturnObject(){
+        IntArrayList ial = new IntArrayList();
+        ial.add(1);
+        ial.add(5);
+        ial.add(54);
+
+        assertEquals(5, ial.get(1));
+    }
+
+    @Test
+    void set() {
+    }
+
+    @Test
+    void testAdd() {
+    }
+
+    @Test
+    void testRemove() {
+    }
+
+    @Test
+    void indexOf() {
+    }
+
+    @Test
+    void lastIndexOf() {
+    }
+
+    @Test
+    void listIterator() {
+    }
+
+    @Test
+    void testListIterator() {
+    }
+
+    @Test
+    void subList() {
+    }
+
+    @Test
+    void retainAll() {
+    }
+
+    @Test
+    void removeAll() {
+    }
+
+    @Test
+    void containsAll() {
+    }
+
+    @Test
+    void testToArray() {
+    }
 }
